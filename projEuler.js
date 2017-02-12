51583072ac5e39025e435bbe14ecb5780812aca0
suhai yehuza <soyehuza@gmail.com>
2017-02-11 22:50:57 -0800

    found a few bugs and fixed them

--- projEuler.js
+++ projEuler.js
@@ -12,7 +12,8 @@ console.log(' ===== 01: MULTIPLES OF 3 AND 5 ===== ');
 //----------------------------------------------------------------------------+
 // Multiples of 3 and 5                                                       +
 // Problem 1                                                                  +
-// If we list all the natural numbers below 10 that are multiples of 3 or 5,  + we get 3, 5, 6 and 9. The sum of these multiples is 23.                       +
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,  +
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.                    + //                                                                            +
 //                                                                            +
 // Find the sum of all the multiples of 3 or 5 below 1000.                    +
 //----------------------------------------------------------------------------+
@@ -40,7 +41,8 @@ console.log(' ===== 02: EVEN FIBONACCI NUMBERS ===== ');
 //                                                                            +
 // 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...                                     +
 //                                                                            +
-// By considering the terms in the Fibonacci sequence whose values do not     + exceed four million, find the sum of the even-valued terms.                   +
+// By considering the terms in the Fibonacci sequence whose values do not     +
+// exceed four million, find the sum of the even-valued terms.                +
 //----------------------------------------------------------------------------+
 var isEven = function (num) {
   return num % 2 === 0;
